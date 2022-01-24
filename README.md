61df5e2601ed027580aa9c21844714ad31bbb400
Dragos Toma <tomadragos96@gmail.com>
2022-01-24 17:00:35 +0200

    ok

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # air_quality
 read data from various senzors
+
+Senzors connected to Raspberry
+DHT 22 (digital; temperature and humidiy)
+BMP085 (digital: temperature and pressure)
+
+Senzors connected to Arduino UNO:
+MQ-2 Gas (analogic ~ 150 mA)
+GP2Y1010AU0F (analogic dust senzor using infrared led - 20 mA)
+
+Arduino is waiting from Raspberry an income message and then collects and send data
+
--- record_data.py
+++ record_data.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from glob import glob
-import time,csv,os,subprocess,re,pdb
+import time,csv,os,subprocess,re,pdb,sys
 import uuid
 
 import pandas as pd
@@ -9,6 +9,7 @@ import serial
 from Adafruit_BMP import BMP085
 import Adafruit_DHT as dht
 
+print('py version',sys.version)
 ##########################
 PERCENT_FROM_AVAILABLE_SPACE = 0.5 # max space to fill with data
 DATA_DIR = './data'
@@ -48,19 +49,18 @@ os.chdir(dname)
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-file1 = DATA_DIR+'/data_sensors.csv'
+file1 = DATA_DIR+'/data_sensors2.csv'
 add_header = not os.path.exists(file1)
 conn1 = open(file1,'a')
 writer1 = csv.writer(conn1)
 if add_header:
-    writer1.writerow(['session_id','timestamp','humidity','temp_dht','pressure','temp_bmp','gas','wifi_conns','90_perc_wifi_power'])
+    writer1.writerow(['session_id','timestamp','humidity','temp_dht','pressure','temp_bmp','wifi_conns','90_perc_wifi_power','dust','gas'])
 #file2 = DATA_DIR+'/data_wifi.csv'
 #conn2 = open(file2,'a')
 #writer2 = csv.writer(conn2)
 
 try:
-    import psutil,sys
-    print('py version',sys.version)
+    import psutil
     hdd = psutil.disk_usage('/')
     free = hdd.free/2**30
     limit = int(PERCENT_FROM_AVAILABLE_SPACE *free)
@@ -70,16 +70,13 @@ except Exception as er:
 print('max GB size allowed of data:',limit)
 
 
-pattern = re.compile('Signal level=(\-\d{1,2}) dBm\s+\n\s+ESSID:\"(.+?)\"')
+wifi_regex = re.compile('Signal level=(\-\d{1,2}) dBm\s+\n\s+ESSID:\"(.+?)\"')
 
 ser = serial.Serial(SERIAL_ADDR, 9600, timeout=1)
 ser.reset_input_buffer()
-serial_regex = re.compile('Value: (\d{1,5}\.\d{1,5}) mill')
+serial_regex = re.compile('ust: (\d+\.\d+) ug\/m3 gas: (\d+\.\d+) en')
 
-# header1
-# timestamp,humiddity,temp_dht,pressure,temp_bmp,gas,wifi_conns,90_perc_wifi_power
-# header2
-# timestamp,power,network
+
 session_id = str(uuid.uuid1())
 while True:
     time.sleep(SLEEP_TIME)
@@ -87,24 +84,23 @@ while True:
     #############################################
     h,t = get_dht22_data()
     temp,pres = get_bmp180_data()
-    gas = 0
+    gas,dust = 0,0
     if COLLECT_ARDUINO_DATA:
         try:
             ser.write(b"Hello from Raspberry Pi!\n")
             line = ser.readline().decode('utf-8').rstrip()
-            gas = str(serial_regex.findall(line)[0])
+	    line = serial_regex.findall(line)
+            gas  = float(str(line[0][1].encode('ascii','ignore')))
+            dust = float(str(line[0][0].encode('ascii','ignore')))
         except Exception as er:
-            gas = 0
-            print(str(er))
-    else:
-        gas = 0
 
+            print(str(er))
     #############################################
     if COLLECT_WIFI_DATA:
         process = subprocess.Popen('sudo iwlist wlan0 scan | egrep "ESSID|Signal"', shell=True, stdout=subprocess.PIPE)
         res = process.stdout.read().decode()
         ll = []
-        for item in pattern.findall(res):
+        for item in wifi_regex.findall(res):
        	    #writer2.writerow([timestamp,item[0],item[1]])
             ll.append(float(item[0]))
         if len(ll)==0:
@@ -114,50 +110,58 @@ while True:
     else:
         ll,power = [],0
     #############################################
-    writer1.writerow([session_id,timestamp,'%.2f'%h,'%.2f'%t,'%.2f'%pres,'%.2f'%temp,gas,len(ll),power])
+    writer1.writerow([session_id,timestamp,'%.2f'%h,'%.2f'%t,'%.2f'%pres,'%.2f'%temp,len(ll),power,dust,gas])
     conn1.flush()            
-    #############################################
-            
+    #############################################            
     tsize = 0
     for filen in glob(DATA_DIR):
     	tsize += os.path.getsize(filen)
     if tsize/1e6>limit*1000:
         print('max size limit reached')
         break
-
     
     
 """
 //ARDUINO CODE
 
-#define MQ2pin (0)
+int measurePin = 1; //Connect dust sensor to Arduino A0 pin
+int gasPin = 0; //MQ -2 senzor
+int ledPower = 2;   //Connect led driver pins of dust sensor to Arduino D2
+int samplingTime = 280;
+int deltaTime = 40;
+int sleepTime = 9680;
+float voMeasured = 0;
+float gasMeasured = 0;
 
-float sensorValue;  //variable to store sensor value
-
-char message[120];
-
-void setup()
-{
-  Serial.begin(9600); // sets the serial port to 9600
-  //Serial.println("Gas sensor warming up!");
-  //delay(20000); // allow the MQ-6 to warm up
+float calcVoltage = 0;
+float dustDensity = 0;
+void setup(){
+  Serial.begin(9600);
+  pinMode(ledPower,OUTPUT);
+  Serial.print("****************** keyestudio ******************\n");
 }
-
-void loop()
-{
-  if (Serial.available() > 0) {
+void loop(){
+if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
-    //Serial.print("You sent me: ");
-    //Serial.println(data);
-      sensorValue = analogRead(MQ2pin); // read analog input pin 0  
-    Serial.print("Sensor Value: ");
-    Serial.print(sensorValue);
-    Serial.print(" millis: ");
-    Serial.print(millis());
-    Serial.println("");
-  }
-
-  //delay(2000); // wait 2s for next reading
+    digitalWrite(ledPower,LOW); // power on the LED
+    delayMicroseconds(samplingTime);
+    voMeasured = analogRead(measurePin); // read the dust value
+    gasMeasured = analogRead(gasPin); // read the MQ value
+    delayMicroseconds(deltaTime);
+    digitalWrite(ledPower,HIGH); // turn the LED off
+    delayMicroseconds(sleepTime);
+    // 0 - 5V mapped to 0 - 1023 integer values
+    // recover voltage
+    calcVoltage = voMeasured * (5.0 / 1024.0);
+    dustDensity = 170 * calcVoltage - 0.1;
+    Serial.print("dust: ");
+    Serial.print(dustDensity);
+    Serial.print(" ug/m3 ");  
+    Serial.print("gas: ");
+    Serial.print(gasMeasured);
+    Serial.print(" end \n");       
+    }   
 }
+//******************************************************************
 """    
 
