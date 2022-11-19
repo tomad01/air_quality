b5501b666418b3990ff7a9a11407d25316b6d6a3
Dragos Toma <tomadragos96@gmail.com>
2022-11-19 08:44:16 +0200

    ok

---
+++ apihelper/sensors.py
@@ -0,0 +1,92 @@
+
+import Adafruit_DHT as dht
+from Adafruit_BMP import BMP085
+import logging
+import re,serial,subprocess
+from apihelper.email import send_email
+import pandas as pd
+
+
+def create_row(collected,header):
+    res = {}
+    for di in collected:
+        res.update(di)
+    return [res[item] for item in header]
+
+class CollectDHT22:
+    def __init__(self):
+        pass
+    def collect(self):
+        try:
+            h,t = dht.read_retry(dht.DHT22, 4)
+        except Exception as er:
+            h,t = 0,0
+        #print(str(er))	
+        return {'humidity':'%.2f'%h,'temp_dht':'%.2f'%t}
+
+class CollectBMP180:
+    def __init__(self):
+        pass
+    def collect(self):
+        try:
+            # i2c on channel 77 (pin 2 data,pin 3 clock)
+            # "sudo i2cdetect -y 1" - to check channel
+            bmp = BMP085.BMP085()
+            temp = bmp.read_temperature()
+            pressure = bmp.read_pressure()
+            #altitude = bmp.read_altitude()
+        except Exception as er:
+        #print(str(er))	
+            temp = 0
+            pressure = 0
+            #altitude = 0
+
+        return {'pressure':'%.2f'%pressure,'temp_bmp':'%.2f'%temp}
+
+
+
+
+class CollectArduino:
+    def __init__(self):
+        SERIAL_ADDR = '/dev/ttyACM0'
+        try:
+            self.ser = serial.Serial(SERIAL_ADDR, 9600, timeout=1)
+            self.ser.reset_input_buffer()
+        except Exception as er:
+            logging.error(str(er))
+        self.serial_regex = re.compile('ust: (\d+\.\d+) ug\/m3 gas: (\d+\.\d+) en')
+    def collect(self):
+    
+        try:
+            self.ser.write(b"Hello from Raspberry Pi!\n")
+            line = self.ser.readline().decode('utf-8').rstrip()
+            line = self.serial_regex.findall(line)
+            gas  = float(str(line[0][1].encode('ascii','ignore')))
+            dust = float(str(line[0][0].encode('ascii','ignore')))
+            if gas>100:
+                send_email('warning gas over 100 %f'%gas)
+        except Exception as er:
+            gas,dust =0,0
+            #logging.error(str(er))
+        return {'dust':dust,'gas':gas}
+
+
+class CollectWIFIdata:
+    def __init__(self):
+        self.wifi_regex = re.compile('Signal level=(\-\d{1,2}) dBm\s+\n\s+ESSID:\"(.+?)\"')
+
+    def collect(self):
+        try:
+            process = subprocess.Popen('sudo iwlist wlan0 scan | egrep "ESSID|Signal"', shell=True, stdout=subprocess.PIPE)
+            res = process.stdout.read().decode()
+            ll = []
+            for item in self.wifi_regex.findall(res):
+                #writer2.writerow([timestamp,item[0],item[1]])
+                ll.append(float(item[0]))
+            if len(ll)==0:
+                ll = [0]        
+            power = round(pd.Series(ll).quantile(.95),2)
+            #conn2.flush()
+        except:
+            ll,power = [],0
+        return {'wifi_conns':len(ll),'90_perc_wifi_power':power}
--- record_data.py
+++ record_data.py
@@ -1,16 +1,13 @@
 from datetime import datetime
 from glob import glob
-import time,csv,os,subprocess,re,pdb,sys
+import time,csv,os,subprocess,sys
 import uuid
 import logging
 import argparse
-import pandas as pd
-import serial
-from Adafruit_BMP import BMP085
-import Adafruit_DHT as dht
 from dotenv import load_dotenv
 load_dotenv()
-from apihelper.email import send_email
+# from apihelper.email import send_email
+import apihelper.sensors as sens
 ##########################
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -23,9 +20,6 @@ args = parser.parse_args()
 PERCENT_FROM_AVAILABLE_SPACE = 0.5 # max space to fill with data
 DATA_DIR = './data'
 SLEEP_TIME = 9
-COLLECT_WIFI_DATA = True
-COLLECT_ARDUINO_DATA = True
-SERIAL_ADDR = '/dev/ttyACM0'
 DATA_FILE_NAME = 'data_sensors_test.csv' if args.debug else 'data_sensors.csv'
 LOG_FILE = 'sensors_test.log' if args.debug else 'sensors.log'
 DEBUG = args.debug
@@ -38,40 +32,15 @@ process = subprocess.Popen('ifconfig | grep wlan', shell=True, stdout=subprocess
 logging.info(process.stdout.read().decode())
 
 
-
-
-def get_dht22_data():
-    try:
-        h,t = dht.read_retry(dht.DHT22, 4)
-    except Exception as er:
-        h,t = 0,0
-	#print(str(er))	
-    return h,t
-
-def get_bmp180_data():
-    try:
-        # i2c on channel 77 (pin 2 data,pin 3 clock)
-        # "sudo i2cdetect -y 1" - to check channel
-        bmp = BMP085.BMP085()
-        temp = bmp.read_temperature()
-        pressure = bmp.read_pressure()
-        #altitude = bmp.read_altitude()
-    except Exception as er:
-	#print(str(er))	
-        temp = 0
-        pressure = 0
-        #altitude = 0
-    return temp,pressure
-
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
 file1 = DATA_DIR+'/'+DATA_FILE_NAME
-add_header = not os.path.exists(file1)
 conn1 = open(file1,'a')
 writer1 = csv.writer(conn1)
-if add_header:
-    writer1.writerow(['session_id','timestamp','humidity','temp_dht','pressure','temp_bmp','wifi_conns','90_perc_wifi_power','dust','gas'])
+header = ['session_id','timestamp','humidity','temp_dht','pressure','temp_bmp','wifi_conns','90_perc_wifi_power','dust','gas']
+if not os.path.exists(file1):
+    writer1.writerow(header)
 #file2 = DATA_DIR+'/data_wifi.csv'
 #conn2 = open(file2,'a')
 #writer2 = csv.writer(conn2)
@@ -87,64 +56,26 @@ except Exception as er:
 logging.info('max GB size allowed of data: '+str(limit))
 
 
-wifi_regex = re.compile('Signal level=(\-\d{1,2}) dBm\s+\n\s+ESSID:\"(.+?)\"')
-try:
-    ser = serial.Serial(SERIAL_ADDR, 9600, timeout=1)
-    ser.reset_input_buffer()
-except Exception as er:
-    logging.error(str(er))
-serial_regex = re.compile('ust: (\d+\.\d+) ug\/m3 gas: (\d+\.\d+) en')
-
-
 session_id = str(uuid.uuid1())
-contor =0
+# contor = 0
+collectors = [sens.CollectDHT22(),sens.CollectBMP180(),sens.CollectArduino(),sens.CollectWIFIdata()]
 while True:
-    if contor==5:
-        process = subprocess.Popen('tail -n 7 %s'%file1, shell=True, stdout=subprocess.PIPE)
-        res = process.stdout.read().decode().encode('ascii','ignore')
-        send_email(res)
+    # if contor==5:
+    #     process = subprocess.Popen('tail -n 7 %s'%file1, shell=True, stdout=subprocess.PIPE)
+    #     res = process.stdout.read().decode().encode('ascii','ignore')
+    #     send_email(res)
     #############################################    
-    time.sleep(SLEEP_TIME)
-    timestamp=datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")
-    #############################################
-    h,t = get_dht22_data()
-    temp,pres = get_bmp180_data()
-    gas,dust = 0,0
-    if COLLECT_ARDUINO_DATA:
-        try:
-            ser.write(b"Hello from Raspberry Pi!\n")
-            line = ser.readline().decode('utf-8').rstrip()
-            line = serial_regex.findall(line)
-            gas  = float(str(line[0][1].encode('ascii','ignore')))
-            dust = float(str(line[0][0].encode('ascii','ignore')))
-            if gas>100:
-                send_email('warning gas over 100 %f'%gas)
-        except Exception as er:
-            pass
-            #logging.error(str(er))
-    #############################################
-    if COLLECT_WIFI_DATA:
-        process = subprocess.Popen('sudo iwlist wlan0 scan | egrep "ESSID|Signal"', shell=True, stdout=subprocess.PIPE)
-        res = process.stdout.read().decode()
-        ll = []
-        for item in wifi_regex.findall(res):
-       	    #writer2.writerow([timestamp,item[0],item[1]])
-            ll.append(float(item[0]))
-        if len(ll)==0:
-            ll = [0]        
-        power = round(pd.Series(ll).quantile(.95),2)
-        #conn2.flush()
-    else:
-        ll,power = [],0
-    #############################################
-    writer1.writerow([session_id,timestamp,'%.2f'%h,'%.2f'%t,'%.2f'%pres,'%.2f'%temp,len(ll),power,dust,gas])
+    time.sleep(SLEEP_TIME)    
+    collected = [cc.collect() for cc in collectors]
+    collected.append({'session_id':session_id,'timestamp':datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")})
+    writer1.writerow(sens.create_row(collected,header))
     conn1.flush()            
     #############################################                
     tsize = sum([os.path.getsize(filen) for filen in glob(DATA_DIR)])
     if tsize/1e6>limit*1000:
         logging.info('max size limit reached')
         break
-    contor +=1
+    # contor +=1
     
     
 """
